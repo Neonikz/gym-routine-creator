e37c141f3fe2cd322500e2248507be88a6a7ea42
Nicolas <heizenredernicolas@gmail.com>
2024-03-12 18:55:41 -0300

    error boundary ready

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/Public/publicRoutes";
+import "./App.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/routes/ErrorBoundary.jsx
+++ src/routes/ErrorBoundary.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import styles from "./styles/styles.module.css";
+import { useRouteError } from "react-router-dom";
 
 export const ErrorBoundary = () => {
-  return <div>ErrorBoundary</div>;
+  const error = useRouteError();
+  return (
+    <div className={styles.container}>
+      <p>
+        Oops <br />
+        ¡Hubo un error!
+      </p>
+      <span>Error: {error.statusText || error.message}</span>
+    </div>
+  );
 };
